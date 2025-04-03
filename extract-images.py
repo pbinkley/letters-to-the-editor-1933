ba84776f499df1a5a5931dde7937e3aac4502158
Peter Binkley <peter.binkley@ualberta.ca>
2025-04-03 10:33:07 -0600

    Initial commit

---
+++ extract-images.py
@@ -0,0 +1,12 @@
+import csv
+from pathlib import PureWindowsPath, PurePosixPath
+
+# copy page blocks from Zotero storage into the project directory
+
+with open('Cleveland Press Letters.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        image_path = str(PurePosixPath(PureWindowsPath(row['File Attachments'])))
+        image_path = image_path.replace("C:\\/Users/pbinkley", "/home/pbinkley/C")
+        output_file = f"{row['Date']}_letters.jpg"
+        print(f"cp '{image_path}' ./raw_images/{output_file}")
