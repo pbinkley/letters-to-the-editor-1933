7c99517a54a2738690e8b2223ee70ab97d032a74
Peter Binkley <peter.binkley@ualberta.ca>
2025-04-04 13:21:03 -0600

    WIP

--- ollama-run-json.py
+++ ollama-run-json.py
@@ -3,37 +3,49 @@ from pydantic import BaseModel
 
 # based on https://ollama.com/blog/structured-outputs
 
+class Letter(BaseModel):
+  Author: str
+  Location: str
+  Subjects: list[str]
+  Summary: str
+
+class LetterList(BaseModel):
+  letters: list[Letter]
+
 with open('prompt_csv.txt', 'r') as file:
     prompt = file.read()
 with open('sample_ocr.txt', 'r') as file:
     ocr_text = file.read()
 
-# prompt ends with <paste OCR text>
-prompt = 'Return as JSON. ' + prompt.replace('<paste OCR text>', ocr_text)
+# loop through letters, 
+# which are separated by empty lines
 
-class Letter(BaseModel):
-  Title: str
-  Author: str
-  Location: int
-  Subjects: list[str]
-  Summary: str
-  LetterText: str
+letters = ocr_text.split("\n\n")
+print(f"There are {len(letters)} letters.")
+# import pdb; pdb.set_trace()
 
-class LetterList(BaseModel):
-  letters: list[Letter]
+for letter in letters:
 
-response = chat(
-    model='llama3.1',
-    messages=[
-      {
-        'role': 'user', 
-        'content': prompt
-      }
-    ],
-    format=LetterList.model_json_schema(),
-)
+  paragraphs = letter.partition('\n')
+  title = paragraphs[0]
+  print("Letter: " + title)
 
-print(response)
+  # prompt ends with <paste OCR text>
+  letter_prompt = 'Return as JSON. ' + prompt.replace('<paste OCR text>', letter)
 
-letters = LetterList.model_validate_json(response.messages.content)
-print(letters)
+  response = chat(
+      model='llama3.1',
+      messages=[
+        {
+          'role': 'user', 
+          'content': letter_prompt
+        }
+      ],
+      format=LetterList.model_json_schema(),
+  )
+
+  print(response)
+  import pdb; pdb.set_trace()
+
+# letters = LetterList.model_validate_json(response.messages.content)
+# print(letters)
